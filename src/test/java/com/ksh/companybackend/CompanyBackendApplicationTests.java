466bd62cbc0c6caf6673462b6b3c992cedfc423c
김성호 <tigertjdgh@gmail.com>
2026-08-10 22:58:24 +0900

    Initial commit

---
+++ src/test/java/com/ksh/companybackend/CompanyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksh.companybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
